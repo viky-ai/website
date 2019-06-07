04e68e360ec5180a8730c6da47dad96ee1b66d10
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2019-06-07 12:17:12 +0200

    #20612 (changed) Cleanup JS code.

--- assets/javascript/doc.js
+++ assets/javascript/doc.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+var App = {
 
-  var buildSummary = function(){
+  buildDocSummary: function(){
     var svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>'
     var h2s = document.querySelectorAll(".doc .article__content h2");
     var html = [];
@@ -18,18 +18,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
       document.querySelector(".doc article .article__summary ul").innerHTML = html.join('');
       document.querySelector(".doc .doc__nav .article__summary ul").innerHTML = html.join('');
     }
-  }
+  },
 
-  var decorateHeadings = function(){
+  decorateDocHeadings: function(){
     var headings = document.querySelectorAll(
       ".doc .article__content h2, .doc .article__content h3, .doc .article__content h4, .doc .article__content h5, .doc .article__content h6"
     );
     Array.prototype.forEach.call(headings, function(heading, i){
       heading.innerHTML = heading.innerHTML + "<a class='anchor' href='#" + heading.id + "'></a>";
     });
-  }
+  },
 
-  var decorateImages = function(){
+  decorateImages: function(){
     var images = document.querySelectorAll(".article__content img");
     Array.prototype.forEach.call(images, function(image, i){
       var html = [];
@@ -43,9 +43,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
       html.push("</figure>");
       image.outerHTML = html.join('');
     });
-  }
+  },
 
-  var setupMobileNav = function(){
+  setupMobileNav: function(){
     document.querySelector("a.header__hamburger").addEventListener("click", function(event){
       event.preventDefault();
       document.body.classList.add('mobile-nav-is-open');
@@ -54,10 +54,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
       event.preventDefault();
       document.body.classList.remove('mobile-nav-is-open');
     });
+  },
+
+  setup: function() {
+    App.buildDocSummary();
+    App.decorateDocHeadings();
+    App.decorateImages();
+    App.setupMobileNav();
   }
 
-  buildSummary();
-  decorateHeadings();
-  decorateImages();
-  setupMobileNav();
+};
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  App.setup();
 });
